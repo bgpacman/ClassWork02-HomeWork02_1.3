84766da64460f2d73572e22f6f255804611ad121
Lubcho Milushev <lubcho.milushev@gmail.com>
2016-12-18 02:44:47 +0200

    ClassWork02-HomeWork02_1.3
    C# 1.3 FULL ClassWork02\HomeWork02

---
+++ TriangleArea1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleArea1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please write the \"b\" value of your triangle: ");
+            decimal bSide = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Please write the \"h\" value of your triangle: ");
+            decimal hSide = decimal.Parse(Console.ReadLine());
+
+            decimal area = (bSide * hSide) / 2;
+            Console.WriteLine("The area of your triangle is: {0}", area);
+        }
+    }
+}
